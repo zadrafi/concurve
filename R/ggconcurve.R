da2935d958245bc0f723a16058a1313be80e9967
Zad Chow <zad@lesslikely.com>
2019-09-18 23:20:29 -0400

    version 2.1.0, double y-axis plotting for ggconcurve( )

--- R/ggconcurve.R
+++ R/ggconcurve.R
@@ -1,9 +1,8 @@
 ggconcurve <- function(type = "consonance", data, measure = "default", nullvalue = "absent", position = "pyramid",
                        title = "Consonance Function",
                        subtitle = "The function contains consonance intervals at every level.",
-                       caption = "Produced with the concurve R package.",
                        xaxis = "Range of Values",
-                       yaxis = "Consonance Level (%)",
+                       yaxis = "P-value",
                        color = "#555555",
                        fill = "#239a98") {
   if (type == "consonance") {
@@ -28,9 +27,6 @@ ggconcurve <- function(type = "consonance", data, measure = "default", nullvalue
     if (is.character(subtitle) != TRUE) {
       stop("Error: 'subtitle' must be a string.")
     }
-    if (is.character(caption) != TRUE) {
-      stop("Error: 'caption' must be a string.")
-    }
     if (is.character(xaxis) != TRUE) {
       stop("Error: 'xaxis' must be a string.")
     }
@@ -41,53 +37,61 @@ ggconcurve <- function(type = "consonance", data, measure = "default", nullvalue
       stop("Error: 'fill' must be a string for the color.")
     }
     ggplot(data = data) +
-      geom_point(aes(x = lower.limit, y = intrvl.level * 100),
+      geom_point(aes(x = lower.limit, y = pvalue),
         color = color, fill = fill, alpha = 0.5, shape = 20, size = 0.1
       ) +
-      geom_point(aes(x = upper.limit, y = intrvl.level * 100),
+      geom_point(aes(x = upper.limit, y = pvalue),
         color = color, fill = fill, alpha = 0.5, shape = 20, size = 0.1
       ) +
-      geom_ribbon(aes(x = lower.limit, ymin = max(intrvl.level * 100), ymax = intrvl.level * 100),
+      geom_ribbon(aes(x = lower.limit, ymin = min(pvalue), ymax = pvalue),
         fill = fill, alpha = 0.30
       ) +
-      geom_ribbon(aes(x = upper.limit, ymin = max(intrvl.level * 100), ymax = intrvl.level * 100),
+      geom_ribbon(aes(x = upper.limit, ymin = min(pvalue), ymax = pvalue),
         fill = fill, alpha = 0.30
       ) +
       labs(
         title = title,
         subtitle = subtitle,
-        caption = caption,
         x = xaxis,
         y = yaxis
       ) +
       theme_light() +
       theme(
-        axis.title.x = element_text(size = 13),
-        axis.title.y = element_text(size = 13)
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12)
       ) + {
         if (measure == "default") scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
       } + {
         if (measure == "ratio") scale_x_log10(breaks = scales::pretty_breaks(n = 10))
       } + {
-        if (position == "inverted") scale_y_continuous(breaks = seq(0, 100, 5), expand = c(0, 0))
+        if (position == "inverted") {
+          scale_y_reverse(
+            breaks = seq(0, 1, .05),
+            sec.axis = sec_axis(~ (1 - .) * 100, name = "Levels for CI (%)", breaks = seq(0, 100, 5))
+          )
+        }
       } + {
-        if (position == "pyramid") scale_y_continuous(trans = "reverse", breaks = seq(0, 100, 5), expand = c(0, 0))
+        if (position == "pyramid") {
+          scale_y_continuous(
+            breaks = seq(0, 1, .05),
+            sec.axis = sec_axis(~ (1 - .) * 100, name = "Levels for CI (%)", breaks = seq(0, 100, 5))
+          )
+        }
       } +
-      theme(text = element_text(size = 15)) +
+      theme(text = element_text(size = 11)) +
       theme(
-        plot.title = element_text(size = 16),
-        plot.subtitle = element_text(size = 12),
-        plot.caption = element_text(size = 8)
+        plot.title = element_text(size = 12),
+        plot.subtitle = element_text(size = 11)
       ) +
       if (nullvalue == "present") {
         if (measure == "default") {
           annotate("segment",
-            x = 0, xend = 0, y = 0, yend = 100,
+            x = 0, xend = 0, y = 0, yend = 1,
             color = "#990000", alpha = 0.3, size = .6
           )
         } else if (measure == "ratio") {
           annotate("segment",
-            x = 1, xend = 1, y = 0, yend = 100,
+            x = 1, xend = 1, y = 0, yend = 1,
             color = "#990000", alpha = 0.3, size = .6
           )
         }
@@ -109,9 +113,6 @@ ggconcurve <- function(type = "consonance", data, measure = "default", nullvalue
     }
     if (is.character(subtitle) != TRUE) {
       stop("Error: 'subtitle' must be a string.")
-    }
-    if (is.character(caption) != TRUE) {
-      stop("Error: 'caption' must be a string.")
     }
     if (is.character(xaxis) != TRUE) {
       stop("Error: 'xaxis' must be a string.")
@@ -136,27 +137,25 @@ ggconcurve <- function(type = "consonance", data, measure = "default", nullvalue
         fill = fill, alpha = 0.30
       ) +
       labs(
-        title = "Surprisal Function",
+        title = title,
         subtitle = subtitle,
-        caption = caption,
         x = xaxis,
         y = "S-value (bits of information)"
       ) +
       theme_light() +
       theme(
-        axis.title.x = element_text(size = 13),
-        axis.title.y = element_text(size = 13)
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12)
       ) + {
         if (measure == "default") scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
       } + {
         if (measure == "ratio") scale_x_log10(breaks = scales::pretty_breaks(n = 10))
       } +
       scale_y_continuous(breaks = seq(0, 14, 0.5), expand = c(0, 0)) +
-      theme(text = element_text(size = 15)) +
+      theme(text = element_text(size = 11)) +
       theme(
-        plot.title = element_text(size = 16),
-        plot.subtitle = element_text(size = 12),
-        plot.caption = element_text(size = 8)
+        plot.title = element_text(size = 12),
+        plot.subtitle = element_text(size = 11)
       )
   }
 }
